6907a40008472a09acc9da245498f4e98da4fd1b
Zhenzh567 <zhenzhg@yandex.ru>
2025-10-08 11:29:15 +0200

    main2.cpp

--- lecture1/main2.cpp
+++ lecture1/main2.cpp
@@ -1,27 +1,24 @@
 #include <iostream>
-int main(){
+int main() {
     int N;
-    std::cin>>N;
-    if (N>=0){
-        switch(N){
-            case 102:std::cout<<"Республика Башкортостан";break;
-            case 11:std::cout<<"Республика Коми";break;
-            case 13:std::cout<<"Республика Мордовия";break;
-            case 15:std::cout<<"Северная Осетия";break;
-            case 116:
-            case 716:std::cout<<"Република Татарстан";break;
-            default:std::cout<<"Информация временно недоступна"<<std::endl;
+    std::cin >> N;
+
+    switch (N) {
+    case 102:std::cout << "Республика Башкортостан"; break;
+    case 11:std::cout << "Республика Коми"; break;
+    case 13:std::cout << "Республика Мордовия"; break;
+    case 15:std::cout << "Северная Осетия"; break;
+    case 116:
+    case 716:std::cout << "Република Татарстан"; break;
+    default:std::cout << "Информация временно недоступна" << std::endl;
 
 
 
-        }
-    }
-    else{
-        std::cout<<"error"<<std::endl;
     }
 
 
 
 
-return 0;
+
+    return 0;
 }
