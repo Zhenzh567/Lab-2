8b2ed48c1a824e138f147fa3092319a11cc6e5bb
Zhenzh567 <zhenzhg@yandex.ru>
2025-10-08 11:28:44 +0200

    main.cpp

--- lecture1/main.cpp
+++ lecture1/main.cpp
@@ -6,36 +6,52 @@ int main() {
     int i;
     std::cin >> A;
     std::cin >> i;
-    if (i<0 || i>7){
-        std::cout<<"Error"<<std::endl;
+    if (i < 0 || i>7) {
+        std::cout << "Error" << std::endl;
     }
-    else{
+    else {
         std::bitset<8>bit(A);
-        std::cout << bit <<"\n";
-        int biti = 1 & (A>>i); 
-        std::cout << biti<<"\n";
-        if (biti==0){
-            unsigned int bit0 = A&1;
-            A ^=(1<<i);
-            A ^= 1;
+        std::cout << bit << "\n";
+        int biti = 1 & (A >> i);
+        std::cout << biti << "\n";
+        if (biti == 0) {
+            unsigned int bit0 = A & 1;
+            A ^= (bit0 << i);
+            A ^= biti;
             std::bitset<8>s(A);
-            std::cout<<s<<std::endl;
+            std::cout << s << std::endl;
 
 
         }
-        else{
+        else {
             int v;
             int b;
             int c;
-            std::cin>>v;
-            std::cin>>b;
-            std::cin>>c;
-            if (abs(v)==abs(b)||abs(v)==abs(c)||abs(c)==abs(b)){
-                std::cout<<"Code zero"<<std::endl;
+            std::cin >> v;
+            std::cin >> b;
+            std::cin >> c;
+            if (abs(v) == abs(b) || abs(v) == abs(c) || abs(c) == abs(b)) {
+                std::cout << "Code zero" << std::endl;
             }
-            else{
-                double z = static_cast<double>(abs(v)+abs(b)+abs(c))/3;
-                std::cout<<z<<std::endl;
+            else {
+                if (abs(v) > abs(b) && abs(v) > abs(c) && abs(b) > abs(c)) {
+                    std::cout << b << std::endl;
+                }
+                if (abs(v) > abs(b) && abs(v) > abs(c) && abs(c) > abs(b)) {
+                    std::cout << c << std::endl;
+                }
+                if (abs(c) > abs(b) && abs(c) > abs(v) && abs(v) > abs(b)) {
+                    std::cout << v << std::endl;
+                }
+                if (abs(c) > abs(b) && abs(c) > abs(v) && abs(b) > abs(v)) {
+                    std::cout << b << std::endl;
+                }
+                if (abs(b) > abs(v) && abs(b) > abs(c) && abs(v) > abs(c)) {
+                    std::cout << v << std::endl;
+                }
+                if (abs(b) > abs(v) && abs(b) > abs(c) && abs(c) > abs(v)) {
+                    std::cout << c << std::endl;
+                }
             }
         }
     }
